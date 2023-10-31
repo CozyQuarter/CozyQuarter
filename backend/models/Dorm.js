5e08bddf9b8216cebb7db5ea4e41704df424cabf
emmaclements <emmamarieclements@gmail.com>
2023-10-31 14:09:20 -0400

    landing page improvements and dorm schema

--- backend/models/Dorm.js
+++ backend/models/Dorm.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const dormSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  id: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true},
+  folder: {type: String, required: true},
+  year: {type: String, required: true},
+  single_price: {type: int, required: true},
+  double_price: {type: int, required: true},
+  triple_price: {type: int, required: true},
+  room_type: {type: String, required: true},
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
 });
 
