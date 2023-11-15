4fd83c67efcd6ad25505ed0feedfaf8ddb29ae31
emmaclements <emmamarieclements@gmail.com>
2023-11-15 18:21:49 -0500

    collapsible changed folders

--- frontend/src/pages/components/FAQ/Collapsible.js
+++ frontend/src/pages/components/FAQ/Collapsible.js
@@ -1,4 +1,15 @@
-// Collapsible sections for the FAQ section
+/**
+ * Collapsible Component
+ * 
+ * A reusable React component for creating collapsible sections. It allows users
+ * to toggle the visibility of content by clicking on a label button.
+ * 
+ * Dependencies:
+ * - React: For creating and rendering the component.
+ * 
+ * Exported Component:
+ * - Collapsible: A React component that provides collapsible sections.
+ */
 
 import React from 'react';
 import { useState } from 'react';
