920effabc7deb7ecb5663aea269813f19c5f8302
emmaclements <emmamarieclements@gmail.com>
2023-11-15 18:18:03 -0500

    improve styling and allow for ratings < 1 stars

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  buildingRating: { type: Number, min: 1, max: 5, required: true },
-  roomRating: { type: Number, min: 1, max: 5, required: true },
-  locationRating: { type: Number, min: 1, max: 5, required: true },
-  cleanlinessRating: { type: Number, min: 1, max: 5, required: true },
-  overallRating: { type: Number, min: 1, max: 5, required: true },
+  buildingRating: { type: Number, min: 0.0, max: 5, required: true },
+  roomRating: { type: Number, min: 0.0, max: 5, required: true },
+  locationRating: { type: Number, min: 0.0, max: 5, required: true },
+  cleanlinessRating: { type: Number, min: 0.0, max: 5, required: true },
+  overallRating: { type: Number, min: 0.0, max: 5, required: true },
   reviewText: { type: String },
 
 });
--- frontend/src/pages/components/FAQ/FAQ.js
+++ frontend/src/pages/components/FAQ/FAQ.js
@@ -14,11 +14,15 @@ const FAQ = () => {
         <div className="panels">
         <div className="left-panel">
             <div className="custom-heading">
-                <b>Resources and FAQ</b>
+                <b>Frequently Asked Questions (FAQ)</b>
             </div>
             <p>
-            Welcome to the comprehensive Resources and FAQ section, designed to enhance your dormitory experience.
-            This page provides links to resources from RPI and answers some common dorm questions.
+            Welcome to the comprehensive FAQ page, designed to inform your on-campus housing experience.
+            This page answers some common dorm questions and provides links to external resources.
+            </p>
+            <p>CozyQuarter is not affiliated with RPI and is not guaranteed to be up to date. To see
+                RPI's official information about dorms, visit the <Link to = "https://sll.rpi.edu/">
+                Office of Student Living and Learning</Link> (SLL) website.
             </p>
         </div>
         <div className="right-panel">
@@ -28,34 +32,41 @@ const FAQ = () => {
 
         <div className="panels">
         <div className="left-panel">
+
             <div className="custom-heading2">
-                <b>External Resources</b>
+                <b>About CozyQuarter</b>
+            </div>
+            <Collapsible label='What is CozyQuarter?'>
+                CozyQuarter is a dorm review website developed by RPI students, for RPI students.
+                It provides a space for RPI students and alumni to review dorms to inform future
+                generations of students.
+            </Collapsible>
+            <Collapsible label='How do I review a dorm?'>
+                To review a dorm, you must first create an account. Go to the Sign In page and enter your
+                name, email, and password. Once you are logged in, you can go to the page for any dorm
+                and write a review. Make sure you follow our <Link to=
+                "/Rules/"onClick={() => { window.scroll(0, 0);}}>guidelines</Link> for posting reviews.
+            </Collapsible>
+            <Collapsible label='Can I delete my review after submitting it?'>
+                Yes, you can delete your reviews. First, make sure you're logged in. Then go to your profile page
+                where you will see a list of all the reviews you've posted. Then delete the review you want
+                to remove. It will no longer appear on the dorm page.
+            </Collapsible>
+
+            <div className="custom-heading2">
+                <b>Housing Application and Pricing</b>
             </div>
 
-            <p> For RPI's official information about dorms, visit the <Link to = "https://sll.rpi.edu/">
-                Office of Student Living and Learning</Link> (SLL).</p>
-
-            <p> <Link to = "https://sll.rpi.edu/housing-comparison"> Housing Comparison</Link>:
-            SLL provides a detailed housing
-            comparison tool to view information about dorms such as room types and amenities</p>
-
-            <p> <Link to = "https://rpi.sodexomyway.com/my-meal-plan"> Meal Plans</Link>:
-            Sodexo provides a list of all meal plans available to RPI students.</p>
-
-            <p> <Link to = "https://sll.rpi.edu/residential-commons/residential-commons-housing-rates">
-            Housing Rates</Link>: SLL provides a list of dorm housing prices by room type. </p>
-        </div>
-
-        <div className="right-panel">
-            <img src={rpi_logo} alt="RPI Logo" className="responsive-image"/>
-        </div>
-        </div>
-
-        <div className="panels">
-        <div className="left-panel">
-            <div className="custom-heading2">
-                <b>Frequently Asked Questions (FAQ)</b>
-            </div>
+            <Collapsible label='How much does on-campus housing cost?'>
+                To view prices for each dorm, visit the page for that dorm on the CozyQuarter website, or visit
+                the <Link to="/AllDorms/"onClick={() => { window.scroll(0, 0);}}> All Dorms</Link> page
+                to see a comprehensive list.
+                <p>
+                    For official pricing information from RPI, SLL provides a list of <Link to =
+                    "https://sll.rpi.edu/residential-commons/residential-commons-housing-rates"> 
+                    dorm prices</Link> by room type.
+                </p>
+            </Collapsible>
 
             <Collapsible label='Are on-campus housing requirements mandatory?'>
                 Yes, RPI students are required to live on campus as freshmen,
@@ -70,32 +81,37 @@ const FAQ = () => {
             <Collapsible label='What is the process for applying for on-campus housing?'>
                 You can apply for on-campus housing through SLL at this
                 link: <Link to = "https://sll.rpi.edu/apply">Housing Application</Link>.
-            </Collapsible>
-
-            <br></br>
-
-            <Collapsible label='What is included in the room and what will I need to bring?'>
-                SLL provides a short recommended packing list specific to RPI: <Link to =
-                "https://sll.rpi.edu/packinglist">Packing List</Link>.
-                <p>For longer packing lists
-                with more detailed tips on living in a dorm for the first time, there are many
-                helpful online resources, such as this article: <Link to = 
-                "https://blog.prepscholar.com/college-packing-list">The Ultimate College
-                Packing List</Link>.</p>
-                <p> SLL also provides a list of prohibited items. Be careful not to bring these
-                    to your dorm, because they can be confiscated or removed: <Link to =
-                    "https://sll.rpi.edu/prohibited-items">Prohibited Items</Link>.
+                <p>
+                    RPI will send you an email with more details when the housing application
+                    opens each semester.
                 </p>
             </Collapsible>
 
-            <Collapsible label='What is the difference between single, double and triple?'>
-                This is the occupancy of the room. Single rooms only house one person, double rooms house two people
-                (you will share the room with one roommate), and triple rooms house three people (you will share
-                the room with two roommates).
+            <div className="custom-heading2">
+                <b>Comparing Dorms</b>
+            </div>
+
+            <Collapsible label='How do I decide what dorm to live in?'>
+                To make an informed decision about what dorm to live in, CozyQuarter is a great
+                resource! You can view the page of any dorm and see reviews from your fellow RPI students
+                and alumni, as well as general information about that dorm such as pricing, room types,
+                and more.
+                <p>
+                    RPI also provides a housing comparison tool to compare different dorms: <Link to =
+                    "https://sll.rpi.edu/housing-comparison">Housing Comparison</Link>.
+                </p>
             </Collapsible>
 
-            <Collapsible label='What are the different room types?'>
-                There are three different room types: Traditional, Suite, and Apartment.
+            <Collapsible label='What is the difference between Single, Double, and Triple?'>
+                This is the occupancy of the room. Single rooms only house one person, double rooms house two people
+                (you will share the room with one roommate), and triple rooms house three people (you will share
+                the room with two roommates). Usually single rooms are the most expensive, and triple rooms are the
+                least expensive.
+            </Collapsible>
+
+            <Collapsible label='What is the difference between Traditional, Suite, and Apartment?'>
+                There are three different dorm types: Traditional, Suite, and Apartment. Some dorms have both suite and
+                traditional options.
                 <p>
                     Traditional rooms have a floor bathroom shared between many dorm rooms, most likely the entire floor.
                 </p>
@@ -112,6 +128,58 @@ const FAQ = () => {
                     or triples.
                 </p>
             </Collapsible>
+
+            <div className="custom-heading2">
+                <b>Dorm Life</b>
+            </div>
+
+            <Collapsible label='What is included in the room and what will I need to bring?'>
+                SLL provides a short recommended packing list specific to RPI: <Link to =
+                "https://sll.rpi.edu/packinglist">Packing List</Link>.
+                <p>For longer packing lists
+                with more detailed tips on living in a dorm for the first time, there are many
+                helpful online resources, such as this article: <Link to = 
+                "https://blog.prepscholar.com/college-packing-list">The Ultimate College
+                Packing List</Link>.</p>
+                <p> SLL also provides a list of prohibited items. Be careful not to bring these
+                    to your dorm, because they can be confiscated or removed: <Link to =
+                    "https://sll.rpi.edu/prohibited-items">Prohibited Items</Link>.
+                </p>
+            </Collapsible>
+            <Collapsible label='How are roommates assigned? Do I get to choose my roommate?'>
+                You can select a preferred roommate on the housing application, although
+                you are not guaranteed to be assigned a room with them. If you don't have a preferred
+                roommate, you can fill out a roommate matching survey during the housing application. Then
+                you will be assigned a roommate based on your responses to the survey.
+            </Collapsible>
+            <Collapsible label='What should I do if I have issues with my roommate?'>
+                You can start by having a conversation with your roommate about your concerns, and make
+                sure to set boundaries. If the issues persist, you can contact your residential assistant.
+                They can intervene and mediate the situation, and possibly escalate to a room change
+                if necessary.
+            </Collapsible>
+
+            <div className="custom-heading2">
+                <b>Related Topics</b>
+            </div>
+
+            <Collapsible label='Am I required to have a meal plan at RPI?'>
+                Yes, if you live in on-campus housing, you are required purchase a meal plan, ranging from 12
+                to unlimited meals per week.
+                <p>
+                    You can find more info about meal plans at <Link to =
+                    "https://rpi.sodexomyway.com/my-meal-plan">Sodexo</Link>.
+                </p>
+            </Collapsible>
+            
+            <p>
+                Do you want to add to the FAQ? Is there a question we didn't answer?
+            </p>
+            <p>
+                Feel free to email the
+                CozyQuarter team at <a href = "mailto: cozyquarterco@gmail.com">cozyquarterco@gmail.com</a>! 
+                We welcome your suggestions and feedback.
+            </p>
 
             <br></br>
             <br></br>
